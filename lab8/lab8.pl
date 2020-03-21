f0e702031137a2609cd78a60eefa874e5c0075ae
karlviik <viik.karl2@gmail.com>
2020-03-21 17:32:53 +0200

    Abstractify

--- lab8/lab8.pl
+++ lab8/lab8.pl
@@ -11,10 +11,11 @@ mortal(X) :-
 lives_in(pompej, marcus).
 lives_in(pompej, francesco).
 
+all_dead(pompej, 79).
+
 birth(marcus, 40).
 birth(jaan, 1977).
 birth(francesco, 1989).
-
 
 naturally_deceased_by(Nimi, Year) :-
 	mortal(Nimi), birth(Nimi, Birth), max_lifespan(mortal, Span), (Birth + Span) < Year.
@@ -25,8 +26,9 @@ alive_at(Nimi, Year) :-
 	not(naturally_deceased_by(Nimi, Year)).
 
 killed(Nimi) :-
-	lives_in(pompej, Nimi),
-	alive_at(Nimi, 79).
+	all_dead(City, Year),
+	lives_in(City, Nimi),
+	alive_at(Nimi, Year).
 
 deceased(Nimi) :-
 	killed(Nimi), ! ;
